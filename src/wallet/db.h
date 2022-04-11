8550f592e796b5ed591d040bdc1395a9df17391a
Andrew Chow <achow101-github@achow101.com>
2022-04-11 17:11:37 -0400

    wallet: Add GetPrefixCursor to DatabaseBatch
    In order to get records beginning with a prefix, we will need a cursor
    specifically for that prefix. So add a GetPrefixCursor function and
    DatabaseCursor classes for dealing with those prefixes.

--- src/wallet/db.h
+++ src/wallet/db.h
@@ -102,6 +102,7 @@ public:
     }
 
     virtual std::unique_ptr<DatabaseCursor> GetCursor() = 0;
+    virtual std::unique_ptr<DatabaseCursor> GetPrefixCursor(CDataStream& prefix) = 0;
     virtual bool TxnBegin() = 0;
     virtual bool TxnCommit() = 0;
     virtual bool TxnAbort() = 0;
@@ -182,6 +183,7 @@ public:
     void Close() override {}
 
     std::unique_ptr<DatabaseCursor> GetCursor() override { return std::make_unique<DummyCursor>(); }
+    std::unique_ptr<DatabaseCursor> GetPrefixCursor(CDataStream& prefix) override { return GetCursor(); }
     bool TxnBegin() override { return true; }
     bool TxnCommit() override { return true; }
     bool TxnAbort() override { return true; }
