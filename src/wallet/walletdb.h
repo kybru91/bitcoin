109404c85b8e23d4503862de004ea0c7cfa993f3
Andrew Chow <achow101-github@achow101.com>
2022-04-13 14:42:00 -0400

    walletdb: Refactor key reading and loading to its own function

--- src/wallet/walletdb.h
+++ src/wallet/walletdb.h
@@ -305,6 +305,8 @@ std::unique_ptr<WalletDatabase> CreateDummyWalletDatabase();
 
 /** Return object for accessing temporary in-memory database. */
 std::unique_ptr<WalletDatabase> CreateMockWalletDatabase();
+
+bool LoadKey(CWallet* pwallet, CDataStream& ssKey, CDataStream& ssValue, std::string& strErr);
 } // namespace wallet
 
 #endif // BITCOIN_WALLET_WALLETDB_H
